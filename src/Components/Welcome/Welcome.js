a96133d3e0a94011c50fd242c6b95fbc5e316c14
nusratnova5 <nusrat.nova16@gmail.com>
2022-10-12 23:41:34 +0600

    project almost done

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -4,14 +4,14 @@ import img from './image.png.jpg'
 const Welcome = () => {
     return (
         <div className='flex justify-center my-5'>
-            <div className='w-3/4'>
+            <div className='w-3/4 bg-violet-100'>
                 <div className='flex items-center '>
                 <div className=''>
                     <img src={img} alt=''></img>
                 </div>
                 <div className=''>
                     <div className='p-5'>
-                    akfjasjkflk
+                    <p className='font-bold text-4xl'>Welcome to <span className=' text-purple-700 font-bold text-4xl'>QuizTaker</span></p>
                     </div>
                 </div>
                 </div>
