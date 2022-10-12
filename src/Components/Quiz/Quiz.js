636f3873b42639043006004bcffab95f4d7f635b
nusratnova5 <nusrat.nova16@gmail.com>
2022-10-12 11:44:29 +0600

    line chart

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -2,22 +2,31 @@ import React from 'react';
 import Option from '../Option/Option';
 import { EyeIcon } from '@heroicons/react/24/solid'
 
-const Quiz = ({quiz}) => {
-    const {question, options, correctAnswer} = quiz;
+const Quiz = ({ quiz }) => {
+    const { question, options, correctAnswer } = quiz;
+
+    const clickedEye = () => {
+        alert(`right ans is ${correctAnswer}`);
+    }
+
     return (
-        <div>
-            <div>
-                <div>{question}</div>
-                <div><EyeIcon className="h-6 w-6"/></div>
+        <div className='flex justify-center'>
+            <div className='my-2'>
+                <div className='flex justify-center'>
+                    <div className='flex'>
+                        <div>{question}</div>
+                        <div onClick={clickedEye} ><EyeIcon className="h-6 w-6" /></div>
+                    </div>
+                </div>
+                <form className='grid grid-cols-2'>
+                    {
+                        options.map(option => <Option
+                            option={option}
+                            correctAnswer={correctAnswer}
+                        ></Option>)
+                    }
+                </form>
             </div>
-            <form className='grid grid-cols-2'>
-            {
-                options.map( option => <Option
-                    option = {option}
-                    correctAnswer = {correctAnswer}
-                ></Option>)
-            }
-            </form>
         </div>
     );
 };
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData().data;
+    console.log(data);
     return (
         <div>
             <h2>This is statistics</h2>
+            <div className='flex justify-center'>
+            <div>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="name" stroke="#82ca9d" />
+                </LineChart>
+            </div>
+            </div>
         </div>
     );
 };
